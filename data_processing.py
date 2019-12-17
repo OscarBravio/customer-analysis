421c485c7fc7e2719bff6dd2779a3a7388e31abb
Oskar <bukowskio@ryanair.com>
2019-12-17 16:32:05 +0100

    modifications

---
+++ data_processing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 16:15:02 2019
+
+@author: bukowskio
+"""
+
+import pandas as pd
+
+orders_df=pd.read_csv("~/Oskar/customer-analysis/olist_orders_dataset.csv")
